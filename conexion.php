bcfb5cd7a28f17b598095def6ca3c3775ee8e143
Ericksoon1 <elerick732@gmail.com>
2024-10-19 20:12:25 -0600

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,15 +1,14 @@
 <?php
-$host = 'localhost';
-$usuario = 'root'; // Usuario de la base de datos
-$contraseña = '';  // Contraseña del usuario
-$base_de_datos = 'videojuegos_db'; // Nombre de la base de datos
- 
+$host = 'servidoriranomas.database.windows.net';  // Nombre del servidor de Azure
+$usuario = 'adminsql';  // Usuario de la base de datos
+$contraseña = 'junioRyzen3200$';  // Contraseña del usuario
+$base_de_datos = 'videojuegos_db';  // Nombre de la base de datos
+
 // Crear la conexión
 $conexion = new mysqli($host, $usuario, $contraseña, $base_de_datos);
- 
+
 // Verificar si la conexión es exitosa
 if ($conexion->connect_error) {
     die('Error de conexión: ' . $conexion->connect_error);
 }
 ?>
- 

4a1f1fccc2b053c527cade40a5498f5937880e0a
Ericksoon1 <elerick732@gmail.com>
2024-10-28 10:45:51 -0600

    Update login.php

--- login.php
+++ login.php
@@ -31,7 +31,7 @@
 <?php
 session_start();
 
-// Conexión a SQL Server
+
 $serverName = "servidoriranomas.database.windows.net";
 $connectionOptions = array(
     "Database" => "videojuegos_db",
@@ -39,10 +39,10 @@ $connectionOptions = array(
     "PWD" => "junioRyzen3200$"
 );
 
-// Establecer la conexión
+
 $conn = sqlsrv_connect($serverName, $connectionOptions);
 
-// Verificar si la conexión fue exitosa
+
 if ($conn === false) {
     die(print_r(sqlsrv_errors(), true));
 }
@@ -51,7 +51,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $username = $_POST['username'];
     $password = $_POST['password'];
 
-    // Consulta SQL utilizando un parámetro preparado para evitar inyecciones SQL
+    
     $query = "SELECT * FROM dbo.users WHERE username = ?";
     $params = array($username);
     $stmt = sqlsrv_query($conn, $query, $params);
@@ -60,10 +60,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         die(print_r(sqlsrv_errors(), true));
     }
 
-    // Obtener los resultados de la consulta
+    
     $user = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC);
 
-    // Verificar si el usuario existe y la contraseña es correcta
+    
     if ($user && password_verify($password, $user['password'])) {
         $_SESSION['user_id'] = $user['id']; 
         header('Location: index.php');
@@ -72,10 +72,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         echo "Usuario o contraseña incorrectos.";
     }
 
-    // Liberar el recurso de la consulta
+    
     sqlsrv_free_stmt($stmt);
 }
 
-// Cerrar la conexión
+
 sqlsrv_close($conn);
 ?>
